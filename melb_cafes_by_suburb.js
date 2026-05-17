4120923249ad94d445d02715f51aa315b0607ba9
Ryan <68524825+SoulWarden1@users.noreply.github.com>
2026-05-17 19:36:19 +1000

    More graphs, web page looks nicer

---
+++ melb_cafes_by_suburb.js
@@ -0,0 +1,19 @@
+var vg_1 = "melb_cafes.vg.json";
+vegaEmbed("#melb_cafes", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "melb_cafes_over_time.vg.json";
+vegaEmbed("#melb_cafes_over_time", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "coffee_prices.vg.json";
+vegaEmbed("#coffee_prices", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "melb_cafes_by_suburb.vg.json";
+vegaEmbed("#melb_cafes_by_suburb", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
