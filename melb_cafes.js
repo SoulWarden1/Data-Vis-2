46e0e83ec784e3035b9fa89bbee1b83beb037a39
Ryan <68524825+SoulWarden1@users.noreply.github.com>
2026-05-18 11:06:52 +1000

    Coffee consumption per capita, moved vega lite files to separate folder

--- melb_cafes.js
+++ melb_cafes.js
@@ -1,29 +1,20 @@
-var vg_1 = "melb_cafes.vg.json";
-vegaEmbed("#melb_cafes", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_1 = "vega-lite/melb_cafes.vg.json";
+vegaEmbed("#melb_cafes", vg_1).then(function(result) {}).catch(console.error);
 
-var vg_2 = "melb_cafes_over_time.vg.json";
-vegaEmbed("#melb_cafes_over_time", vg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_2 = "vega-lite/melb_cafes_over_time.vg.json";
+vegaEmbed("#melb_cafes_over_time", vg_2).then(function(result) {}).catch(console.error);
 
-var vg_3 = "coffee_prices.vg.json";
-vegaEmbed("#coffee_prices", vg_3).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_3 = "vega-lite/coffee_prices.vg.json";
+vegaEmbed("#coffee_prices", vg_3).then(function(result) {}).catch(console.error);
 
-var vg_4 = "melb_cafes_by_suburb.vg.json";
-vegaEmbed("#melb_cafes_by_suburb", vg_4).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_4 = "vega-lite/melb_cafes_by_suburb.vg.json";
+vegaEmbed("#melb_cafes_by_suburb", vg_4).then(function(result) {}).catch(console.error);
 
-var vg_5 = "melb_cafes_by_suburb_per_capita.vg.json";
-vegaEmbed("#melb_cafes_by_suburb_per_capita", vg_5).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_5 = "vega-lite/melb_cafes_by_suburb_per_capita.vg.json";
+vegaEmbed("#melb_cafes_by_suburb_per_capita", vg_5).then(function(result) {}).catch(console.error);
 
-var vg_6 = "top_suburbs_per_capita.vg.json";
-vegaEmbed("#top_suburbs_per_capita", vg_6).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_6 = "vega-lite/top_suburbs_per_capita.vg.json";
+vegaEmbed("#top_suburbs_per_capita", vg_6).then(function(result) {}).catch(console.error);
+
+var vg_7 = "vega-lite/coffee_consumption_per_capita.vg.json";
+vegaEmbed("#coffee_consumption_per_capita", vg_7).then(function(result) {}).catch(console.error);
