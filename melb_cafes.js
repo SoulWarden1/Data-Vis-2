4b6c24349d2151de94a5cfbc010b5e0f71867661
Ryan <68524825+SoulWarden1@users.noreply.github.com>
2026-05-18 10:43:19 +1000

    More graphs

---
+++ melb_cafes.js
@@ -0,0 +1,29 @@
+var vg_1 = "melb_cafes.vg.json";
+vegaEmbed("#melb_cafes", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "melb_cafes_over_time.vg.json";
+vegaEmbed("#melb_cafes_over_time", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "coffee_prices.vg.json";
+vegaEmbed("#coffee_prices", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "melb_cafes_by_suburb.vg.json";
+vegaEmbed("#melb_cafes_by_suburb", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "melb_cafes_by_suburb_per_capita.vg.json";
+vegaEmbed("#melb_cafes_by_suburb_per_capita", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "top_suburbs_per_capita.vg.json";
+vegaEmbed("#top_suburbs_per_capita", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
